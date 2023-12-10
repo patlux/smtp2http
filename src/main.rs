12c08bbebd2449de7e95cda0709ca093ede8308c
Patrick Wozniak <email@patwoz.de>
2023-12-10 13:45:19 +0100

    forward incoming mails to given http endpoint via post

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ enum Commands {
     Server {
         #[structopt(default_value = "127.0.0.1:25")]
         listen: String,
+
+        #[structopt(default_value = "http://localhost:1337")]
+        endpoint: String,
     },
     Send {
         #[structopt(default_value = "smtp://127.0.0.1:25")]
@@ -18,18 +21,30 @@ enum Commands {
 }
 
 #[derive(Clone)]
-struct HttpForwarderHandler {
+struct Smtp2HttpHandler {
     pub data: Vec<u8>,
+    pub endpoint: String,
 }
 
-impl Handler for HttpForwarderHandler {
+impl Handler for Smtp2HttpHandler {
     fn data(&mut self, buf: &[u8]) -> std::io::Result<()> {
-        self.data.append(&mut buf.to_owned());
+        self.data.extend_from_slice(buf);
         Ok(())
     }
     fn data_end(&mut self) -> mailin::Response {
         let message = String::from_utf8(self.data.to_vec()).unwrap();
-        info!("{}", message);
+
+        let client = reqwest::blocking::Client::new();
+        let result = client
+            .post(&self.endpoint)
+            .body(serde_json::json!({ "hello": "sir" }).to_string())
+            .send();
+
+        match result {
+            Ok(_) => println!("Sent message to {}.", &self.endpoint),
+            Err(err) => println!("Sending not successful. {}", err),
+        }
+
         OK
     }
 }
@@ -60,9 +75,11 @@ async fn main() -> anyhow::Result<()> {
                 Err(e) => panic!("Could not send email: {e:?}"),
             }
         }
-        Commands::Server { listen } => {
-            let smtp2http_handler = HttpForwarderHandler { data: vec![] };
-            let mut server = Server::new(smtp2http_handler);
+        Commands::Server { listen, endpoint } => {
+            let mut server = Server::new(Smtp2HttpHandler {
+                data: vec![],
+                endpoint,
+            });
 
             server
                 .with_name("localhost")
